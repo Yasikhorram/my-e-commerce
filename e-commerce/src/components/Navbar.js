fa58ffbceb0716c7e134756d12e43c7aa796397b
Yasikhorram <khorramyasaman@gmail.com>
2022-02-28 22:52:29 -0600

    add search icon from material ui

--- e-commerce/src/components/Navbar.js
+++ e-commerce/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Container = styled.div`
   height: 60px;
@@ -8,16 +9,35 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  justify-content: space-between;
 `;
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+  flex: 1;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 1px solid blue;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>Left</Left>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <SearchIcon />
+          </SearchContainer>
+        </Left>
         <Center>Center</Center>
         <Right>Right</Right>
       </Wrapper>
